6083faf92264e966505ee9fdb3ab784e48ac4a10
Saga Åkermark <sagaakermark@hotmail.com>
2019-09-17 18:09:38 +0200

    added image upload and first css styling

--- models/Post-form.js
+++ models/Post-form.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-  img: String,
   title: String,
   description: String,
   artist: String,
   area: String,
   adress: String,
-  postBy: String
+  postBy: String,
+  img: String,
 });
 
 const Post = mongoose.model("Post", postSchema);
--- routes/explore.js
+++ routes/explore.js
@@ -1,19 +1,40 @@
 const express = require("express");
 const router = express.Router();
-
+const uploadCloud = require('../config/cloudinary.js');
 const Post = require("../models/Post-form");
 
 //  GET /explore
 router.get("/", (req, res, next) => {
   // gets the wall collection
   // render the explore view with the wall data
-  res.render("explore");
+  // ?????
+  Picture.find((err, pictures) => {
+    res.render("explore", {
+      pictures
+    })
+  })
 });
 
 //  POST /explore
-router.post("/", (req, res, next) => {
-  let { title, artist, description, area, adress, postBy } = req.body;
-  Post.create({ title, artist, description, area, adress, postBy }).then(() => {
+router.post("/", uploadCloud.single("photo"), (req, res, next) => {
+  console.log(req.body)
+  let {
+    title,
+    artist,
+    description,
+    area,
+    adress,
+    postBy
+  } = req.body;
+  Post.create({
+    title,
+    artist,
+    description,
+    area,
+    adress,
+    postBy,
+    img: req.file.url
+  }).then(() => {
     res.send("created");
   });
   // gets the wall collection
@@ -26,5 +47,6 @@ router.get("/postForm", (req, res, next) => {
   // render the explore view with the wall data
   res.render("postForm");
 });
+
 
 module.exports = router;
